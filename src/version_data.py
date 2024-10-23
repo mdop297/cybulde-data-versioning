921b9771d605796bc61d1b94d4584108b5a5600b
mdop297 <lenhatminh297@gmail.com>
2024-10-23 14:11:30 +0700

    added initialize DVC funtions

--- src/version_data.py
+++ src/version_data.py
@@ -1,11 +1,12 @@
 from src.config_schemas.config_schema import Config
 from src.utils.config_utils import get_config
+from src.utils.data_utils import initialize_dvc
 
 
 @get_config(config_path="../configs", config_name="config")
-def entrypoint(config: Config) -> None:
-    print(config)
+def version_data(config: Config) -> None:
+    initialize_dvc()
 
 
 if __name__ == "__main__":
-    entrypoint()  # type: ignore
+    version_data()
