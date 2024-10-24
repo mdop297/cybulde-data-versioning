57ec6dce52b0e81c94584f1cb753f9f18732268f
mdop297 <lenhatminh297@gmail.com>
2024-10-24 15:13:24 +0700

    Updated version of the data from v0 to v1

--- src/version_data.py
+++ src/version_data.py
@@ -1,6 +1,6 @@
 from src.config_schemas.config_schema import Config
 from src.utils.config_utils import get_config
-from src.utils.data_utils import initialize_dvc, initialize_dvc_storage
+from src.utils.data_utils import initialize_dvc, initialize_dvc_storage, make_new_data_version
 
 
 @get_config(config_path="../configs", config_name="config")
@@ -8,6 +8,7 @@ def version_data(config: Config) -> None:
     print(config)
     initialize_dvc()
     initialize_dvc_storage(dvc_remote_name=config.dvc_remote_name, dvc_remote_url=config.dvc_remote_url)
+    make_new_data_version(dvc_raw_data_folder=config.dvc_raw_data_folder, dvc_remote_name=config.dvc_remote_name)
 
 
 if __name__ == "__main__":
